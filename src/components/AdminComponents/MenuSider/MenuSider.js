d18f9c0ab71d16e70d780b10a04c17ec5ecb64f0
Ricardo <richy.2603@gmail.com>
2022-05-14 12:20:16 -0500

    cambios

--- src/components/AdminComponents/MenuSider/MenuSider.js
+++ src/components/AdminComponents/MenuSider/MenuSider.js
@@ -18,7 +18,7 @@ export default function MenuSider(props){
                     </Link>
                 </Menu.Item>
 
-                <Menu.Item key="1">
+                <Menu.Item key="2">
                     <Link to={"/admin/menu-web"}>
                         <MenuOutlined/>
                         <span className="nav-text">Menu Web</span>
